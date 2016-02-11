064621067a85c328d645b08676c28ddb6b15ed0d
Jongho Moon <jongho.moon@navercorp.com>
2016-02-11 17:39:05 +0900

    Initial Commit

---
+++ src/main/java/com/navercorp/pinpoint/plugin/template/MyPlugin.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2016 NAVER Corp.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.navercorp.pinpoint.plugin.template;
+
+import com.navercorp.pinpoint.bootstrap.instrument.transformer.TransformTemplate;
+import com.navercorp.pinpoint.bootstrap.instrument.transformer.TransformTemplateAware;
+import com.navercorp.pinpoint.bootstrap.plugin.ProfilerPlugin;
+import com.navercorp.pinpoint.bootstrap.plugin.ProfilerPluginSetupContext;
+
+public class MyPlugin implements ProfilerPlugin, TransformTemplateAware {
+    private TransformTemplate transformTemplate;
+    
+    @Override
+    public void setup(ProfilerPluginSetupContext context) {
+        
+    }
+
+    @Override
+    public void setTransformTemplate(TransformTemplate transformTemplate) {
+        this.transformTemplate = transformTemplate;
+    }
+}
---
+++ src/test/java/com/navercorp/pinpoint/plugin/template/TestConstants.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright 2016 NAVER Corp.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.navercorp.pinpoint.plugin.template;
+
+/**
+ * @author Jongho Moon
+ *
+ */
+public interface TestConstants {
+    public static final String VERSION = "1.5.2-SNAPSHOT";
+    public static final String AGENT_PATH = "target/plugin-template-" + VERSION;
+}
